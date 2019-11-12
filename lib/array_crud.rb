1b04acb6b52c0ccd72c44b78ae68fa64db5f0c20
Romario Gustave <romagustave10@gmail.com>
2019-11-12 03:19:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,17 +23,20 @@ end
 
 def remove_element_from_start_of_array(array)
   array = ["bob","bobby"]
-  array.
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array =["lol" 5]
+  array[1]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array =["tall", 4]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[1,2,3,4,5,6,7,8,9]
+  array[-1]
 end
