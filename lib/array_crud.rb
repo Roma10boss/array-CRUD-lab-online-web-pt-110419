1cab689c22584910143e35c7fc9f287dcd8f8b7a
Romario Gustave <romagustave10@gmail.com>
2019-11-12 03:18:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,11 +18,12 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["l","lovely"]
-  array.shift[0]
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = ["bob","bobby"]
+  array.
 end
 
 def retrieve_element_from_index(array, index_number)
